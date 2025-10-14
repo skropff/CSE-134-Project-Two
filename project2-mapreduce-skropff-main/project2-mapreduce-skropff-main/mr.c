74518cf315200bd4f23ea06e325e65a0296db182
Sean Nicholas Kropff <117237260+skropff@users.noreply.github.com>
2025-10-14 12:17:49 -0700

    Update mr.c

--- project2-mapreduce-skropff-main/project2-mapreduce-skropff-main/mr.c
+++ project2-mapreduce-skropff-main/project2-mapreduce-skropff-main/mr.c
@@ -145,6 +145,7 @@ void map_reduce(mapper_t mapper, size_t num_mapper, reducer_t reducer,
   kvlist_t *lists4[num_reducer];
   for (int i = 0; i < (int) num_reducer; i = i + 1) {
     lists3[i] = kvlist_new();
+    lists4[i] = kvlist_new();
   }
   for (int i = 0; i < (int) num_mapper; i = i + 1) {
     current = get_head(lists2[i]);
